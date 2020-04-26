797e5ec3c2a42efa11768b4ad51510d47a948caa
Pofay <gianCarlo.gilos@gmail.com>
2020-04-26 21:27:23 +0800

    Implementing Pathfinding for Waypoints

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavePointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<WaypointsContainer>().points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/WaypointsContainer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsContainer : MonoBehaviour
+{
+    public Transform[] points;
+
+    private void Awake()
+    {
+        InitializeWaypoints();
+    }
+
+    private void InitializeWaypoints()
+    {
+        points = new Transform[transform.childCount];
+        for (var i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+}
