7a7cb4893555b228e2cac43c556f538cb9f834cb
Pofay <gianCarlo.gilos@gmail.com>
2020-04-26 21:48:30 +0800

    Implemented Ad-hoc Pathfinding

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,16 +8,35 @@ public class EnemyMovement : MonoBehaviour
     public float speed = 10f;
     private Transform target;
     private int wavePointIndex = 0;
+    private WaypointsContainer waypoints;
     // Start is called before the first frame update
     void Start()
     {
-        target = FindObjectOfType<WaypointsContainer>().points[0];
+        waypoints = FindObjectOfType<WaypointsContainer>();
+        target = waypoints.points[0];
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 direction = target.position - transform.position;
-        transform.Translate(direction.normalized * speed * Time.deltaTime);
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (wavePointIndex < waypoints.points.Length - 1)
+        {
+            wavePointIndex++;
+            target = waypoints.points[wavePointIndex];
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
