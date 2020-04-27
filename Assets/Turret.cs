c130a5ee5c82c0fcdb07b35e054f7417db8be7bc
Pofay <gianCarlo.gilos@gmail.com>
2020-04-27 20:08:01 +0800

    Turret now rotates to nearest enemy in range

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -5,18 +5,49 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
 
-    public Transform target;
+    private Transform target;
     public float range = 15f;
+    public string targetTag = "Enemy";
+    public Transform partToRotate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(targetTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach(var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null) return;
+
+        var dir = target.position - transform.position;
+        var lookRotation = Quaternion.LookRotation(dir);
+        var rotation = lookRotation.eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
 
     private void OnDrawGizmosSelected()
