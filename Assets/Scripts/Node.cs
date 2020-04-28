0cf6f939ad9d981ea3a54f77b7e9170e37bff33f
Pofay <gianCarlo.gilos@gmail.com>
2020-04-28 15:59:07 +0800

    Added hover effect on nodes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+
+    private Color startColor;
+    private Renderer materialRenderer;
+    
+    private void OnMouseEnter()
+    {
+        materialRenderer.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        materialRenderer.material.color = startColor;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        materialRenderer = GetComponent<Renderer>();
+        startColor = materialRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
