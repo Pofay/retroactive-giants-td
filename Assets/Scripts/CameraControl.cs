5989b0b5972ab61722f81ceb10905436baf3659a
Pofay <gianCarlo.gilos@gmail.com>
2020-04-28 17:12:24 +0800

    Added Camera movement and scrolling

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    public float scrollSpeed = 5f;
+
+    private bool doMovement = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            doMovement = !doMovement;
+        }
+        if (!doMovement)
+            return;
+
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        var currentPos = transform.position;
+        Debug.Log(scroll);
+
+        currentPos.y -= (scroll * 1000) * scrollSpeed * Time.deltaTime;
+        transform.position = currentPos;
+    }
+}
