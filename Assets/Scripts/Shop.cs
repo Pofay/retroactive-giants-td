0eaca26fb721c1ffa9b0139a3810edb771967afa
Pofay <gianCarlo.gilos@gmail.com>
2020-04-29 13:54:09 +0800

    Created Script to control TurretConstructor based on Selected turret in UI

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    private TurretConstructor turretConstructor;
+
+    private void Awake()
+    {
+        turretConstructor = FindObjectOfType<TurretConstructor>();
+    }
+    public void PurchaseStandardTurret()
+    {
+        Debug.Log("Standard Turret Purchased");
+        turretConstructor.SetTurretToConstruct(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
