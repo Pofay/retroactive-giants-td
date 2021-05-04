270596587e27fc2b3c056675e3d82eba099e4c30
Pofay <gianCarlo.gilos@gmail.com>
2021-05-04 17:11:47 +0800

    Added LivesRemover to forward calls to PlayerStats.RemoveLives

---
+++ Assets/Scripts/LivesRemover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesRemover : MonoBehaviour
+{
+    private PlayerStats playerStats;
+
+    void Awake()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    public void RemoveLives(int amount)
+    {
+        playerStats.RemoveLives(amount);
+    }
+}
