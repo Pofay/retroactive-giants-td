9f4ea740dc3dddebbc1ec631ad3c471e063ceaf8
Pofay <gianCarlo.gilos@gmail.com>
2021-05-05 16:02:39 +0800

    Can now show current lives and currency

---
+++ Assets/Scripts/PlayerStatisticsUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStatisticsUI : MonoBehaviour
+{
+    public TextMeshProUGUI currencyText;
+    public TextMeshProUGUI livesText;
+
+    [SerializeField] private PlayerStats stats;
+
+    void Awake()
+    {
+        stats = GetComponent<PlayerStats>();
+        stats.OnLivesChange += ShowLives;
+        stats.OnCurrencyChange += ShowCurrency;
+    }
+
+    private void ShowCurrency(int currency)
+    {
+        currencyText.text = string.Format("Currency: {0}", currency);
+    }
+
+    private void ShowLives(int lives)
+    {
+        livesText.text = string.Format("Lives: {0}", lives);
+    }
+}
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,34 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    public event Action<int> OnLivesChange;
+    public event Action<int> OnCurrencyChange;
+
     [Header("Player Statistics")]
     [SerializeField] private int currency = 500;
     [SerializeField] private int lives = 20;
+
+    void Awake()
+    {
+        OnLivesChange?.Invoke(lives);
+        OnCurrencyChange?.Invoke(currency);
+    }
 
     public void ReduceCurrency(int amount)
     {
         if (currency > 0)
         {
             currency -= amount;
+            OnCurrencyChange?.Invoke(currency);
         }
     }
 
     public void AddCurrency(int amount)
     {
         currency += amount;
+        OnCurrencyChange?.Invoke(currency);
     }
 
     public void AddLives(int amount)
     {
         lives += amount;
+        OnLivesChange?.Invoke(lives);
     }
 
     public void RemoveLives(int amount)
     {
-        if(lives > 0)
+        if (lives > 0)
         {
             lives -= amount;
+            OnLivesChange?.Invoke(lives);
         }
     }
 
