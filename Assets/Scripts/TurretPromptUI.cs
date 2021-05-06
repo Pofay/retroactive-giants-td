9578d696f3182d4a1bc87b3d446dd53350d2cdec
Pofay <gianCarlo.gilos@gmail.com>
2021-05-06 13:54:39 +0800

    Added functions to enable dynamic listeners on sell button

--- Assets/Scripts/TurretPromptUI.cs
+++ Assets/Scripts/TurretPromptUI.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretPromptUI : MonoBehaviour
 {
     [SerializeField] private Transform canvas;
+    [SerializeField] private Button sellButton;
 
     public void Show()
     {
@@ -17,5 +19,15 @@ public class TurretPromptUI : MonoBehaviour
     public void TransferPosition(Vector3 targetPosition)
     {
         this.gameObject.transform.position = targetPosition;
+    }
+
+    public void AttachButtonEvents(Node n)
+    {
+        sellButton.onClick.AddListener(() => n.SellTurret());
+    }
+
+    public void DetachButtonEvents()
+    {
+        sellButton.onClick.RemoveAllListeners();
     }
 }
