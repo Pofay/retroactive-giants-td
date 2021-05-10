6df0e54682eb55ae7f821bb5f5e3b0b8e249479a
Pofay <gianCarlo.gilos@gmail.com>
2021-05-10 18:00:12 +0800

    Can now retry the game from a game over

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverMenu : MonoBehaviour
 {
     public GameObject gameOverScreen;
+    public Button retryButton;
+    public Button mainMenuButton;
 
     private PlayerStats player;
 
@@ -11,11 +15,24 @@ public class GameOverMenu : MonoBehaviour
         player = GetComponent<PlayerStats>();
         gameOverScreen.SetActive(false);
         player.OnLivesEmptied += TriggerGameOver;
+        retryButton.onClick.AddListener(() => Retry());
+        mainMenuButton.onClick.AddListener(() => ToMenu());
     }
 
     void TriggerGameOver()
     {
         gameOverScreen.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void ToMenu()
+    {
+        Debug.Log("To Implement Main Menu Scene and Screen");
     }
 }
