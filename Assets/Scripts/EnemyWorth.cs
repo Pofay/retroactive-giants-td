4528ca3c0aed069b20ef31db0ef66fe5858d3c9f
Pofay <gianCarlo.gilos@gmail.com>
2021-05-10 14:32:27 +0800

    Added enemy worth to give player currency or lives on enemy kill

---
+++ Assets/Scripts/EnemyWorth.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnemyWorth : MonoBehaviour
+{
+    public int currencyOnKill = 25;
+    public int livesOnKill = 0;
+
+    void OnDestroy()
+    {
+        FindObjectOfType<PlayerStats>().AddCurrency(currencyOnKill);
+    }
+
+}
