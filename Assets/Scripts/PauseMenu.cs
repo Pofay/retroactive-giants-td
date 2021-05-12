e0fd77dcba20d6ff8a9f8257d74352fadac4694b
Pofay <gianCarlo.gilos@gmail.com>
2021-05-12 10:55:59 +0800

    Can now pause the game with specific button functions working

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public Button continueButton;
+    public Button mainMenuButton;
+    public Button retryButton;
+
+    void Awake()
+    {
+        pauseScreen.SetActive(false);
+        continueButton.onClick.AddListener(() => Unpause());
+        mainMenuButton.onClick.AddListener(() => ToMainMenu());
+        retryButton.onClick.AddListener(() => Retry());
+    }
+
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (!pauseScreen.activeSelf)
+            {
+                Time.timeScale = 0f;
+                pauseScreen.SetActive(true);
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1f;
+        pauseScreen.SetActive(false);
+    }
+
+    void ToMainMenu()
+    {
+        Debug.Log("To Implement Main Menu Scene");
+    }
+}
