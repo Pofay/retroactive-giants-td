48b98d843d27d7502fdaac9d7eae910046992e6c
Pofay <gianCarlo.gilos@gmail.com>
2021-05-14 14:54:45 +0800

    Minor Cleanup

--- Assets/Scripts/LevelUnlocker.cs
+++ Assets/Scripts/LevelUnlocker.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LevelUnlocker
 {
-
     public void UnlockLevel(int levelIndex)
     {
         var currentUnlockedLevels = PlayerPrefs.GetInt("maxLevelReached", 0);
