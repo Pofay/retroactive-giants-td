49489d2bff0e2aa576d81dd1ee3411c87875073e
Pofay <gianCarlo.gilos@gmail.com>
2021-05-18 21:15:19 +0800

    Provided capability to go to next level or go to level select

---
+++ Assets/Scripts/ContinueScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ContinueScreen : MonoBehaviour
+{
+    public GameObject continueScreen;
+    public Button toNextLevelButton;
+    public Button toLevelSelectionButton;
+
+    private WaveSpawner spawner;
+
+    void Awake()
+    {
+        continueScreen.SetActive(false);
+        spawner = FindObjectOfType<WaveSpawner>();
+        spawner.OnAllEnemiesDisabled += ShowWinScreen;
+        toNextLevelButton.onClick.AddListener(() => ToNextLevel());
+        toLevelSelectionButton.onClick.AddListener(() => ToLevelSelection());
+    }
+
+    void ToLevelSelection()
+    {
+        continueScreen.SetActive(false);
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    void ToNextLevel()
+    {
+        SceneManager.LoadScene(string.Format("Level{0}", spawner.levelToUnlock + 1));
+    }
+
+    void ShowWinScreen()
+    {
+        continueScreen.SetActive(true);
+    }
+
+
+}
