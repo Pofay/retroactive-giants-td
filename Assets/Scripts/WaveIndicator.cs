e89d9d526c57918ef11d968da2b19ab3da93d052
Pofay <gianCarlo.gilos@gmail.com>
2021-06-01 07:54:17 +0800

    Added WaveIndicator prefab to show player current wave and knowing how many waves a level has

---
+++ Assets/Scripts/WaveIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveIndicator : MonoBehaviour
+{
+    [SerializeField] private Image waveLevelSlider;
+    [SerializeField] private TextMeshProUGUI waveLevelText;
+
+    void Awake()
+    {
+        var spawner = FindObjectOfType<WaveSpawner>();
+        spawner.OnWaveChanged += ShowWaveLevel;
+    }
+
+    private void ShowWaveLevel(int currentWaveLevel, int maxWaveLevel)
+    {
+        waveLevelText.text = string.Format("{0}/{1}", currentWaveLevel, maxWaveLevel);
+
+        waveLevelSlider.fillAmount = (float) currentWaveLevel / maxWaveLevel;
+    }
+}
