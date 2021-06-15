89c4bd7661e81e378feb0381cddb1d2d94ab83f5
Pofay <gianCarlo.gilos@gmail.com>
2021-06-15 22:02:44 +0800

    Earth Golem 1 now has walk animation

---
+++ Assets/Scripts/EnemyLocomotionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLocomotionController : MonoBehaviour
+{
+    public float locomotionSmoothing = 0.1f;
+
+    private Animator animator;
+    private EnemyMovement movement;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        movement = GetComponent<EnemyMovement>();
+    }
+
+    void Update()
+    {
+        var speed = transform.position.magnitude / movement.speed;
+        animator.SetFloat("speed", speed, locomotionSmoothing, Time.deltaTime); 
+    }
+}
