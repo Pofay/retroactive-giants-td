0b15101c6eb766a7fef53e71ffa93455ccfb6c59
Pofay <gianCarlo.gilos@gmail.com>
2021-06-19 23:19:06 +0800

    Demoted HealthUI script to HeathUI component in golem prefabs to allow health bars to face camera

--- Assets/Scripts/EnemyHealthUI.cs
+++ Assets/Scripts/EnemyHealthUI.cs
@@ -7,17 +7,24 @@ using UnityEngine.UI;
 public class EnemyHealthUI : MonoBehaviour
 {
     [SerializeField] private Image healthSlider;
-
     private EnemyHealth health;
+    private Transform cam;
 
     void Awake()
     {
-        health = GetComponent<EnemyHealth>();
+        health = GetComponentInParent<EnemyHealth>();
     }
 
     void Start()
     {
+        cam = Camera.main.transform;
         health.OnHealthChanged += DisplayCurrentHealth;
+    }
+
+    void LateUpdate()
+    {
+        var lookDirection = new Vector3(cam.position.x, transform.position.y, cam.position.z);
+        transform.LookAt(lookDirection, Vector3.down);
     }
 
     private void DisplayCurrentHealth(float currentHealth, float maxHealth)
