79d12ff7919bf9eae8e4aa3d70a55cd17ebd6e74
Pofay <gianCarlo.gilos@gmail.com>
2021-06-28 19:55:48 +0800

    Created initial implementation for Flamethrower turret

---
+++ Assets/Scripts/ParticleEmittingTurret.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class ParticleEmittingTurret : Turret
+{
+    [SerializeField] private ParticleSystem flames;
+
+    protected override void Shoot()
+    {
+        if (!flames.isPlaying)
+        {
+            flames.Play();
+        }
+    }
+
+    protected override void Update()
+    {
+        if (target == null)
+        {
+            DisableFlames();
+            return;
+        }
+        else
+        {
+            targeting.LookAtCurrentTarget(target);
+            Shoot();
+        }
+    }
+
+    private void DisableFlames()
+    {
+        flames.Stop();
+    }
+}
