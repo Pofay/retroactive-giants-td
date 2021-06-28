635bd26c34e265425ac23d8428d36ba868b34694
Pofay <gianCarlo.gilos@gmail.com>
2021-06-28 20:40:28 +0800

    Added particle collision receiver so that golems can now be damaged by flamethrower particles

---
+++ Assets/Scripts/EnemyParticleEmissionReceiver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyParticleEmissionReceiver : MonoBehaviour
+{
+    private EnemyHealth health;
+    private bool isReadyToReceiveDamage;
+    private bool isCoroutineStarted;
+
+    void Start()
+    {
+        health = GetComponent<EnemyHealth>();
+        isReadyToReceiveDamage = true;
+
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        var turret = other.GetComponentInParent<ParticleEmittingTurret>();
+        if (isReadyToReceiveDamage)
+        {
+            health.TakeDamage(turret.damage);
+            isReadyToReceiveDamage = false;
+        }
+        else
+        {
+            if (!isCoroutineStarted)
+            {
+                StartCoroutine(StopReceivingUntilInterval(turret.damageInterval));
+            }
+        }
+    }
+
+    private IEnumerator StopReceivingUntilInterval(float damageInterval)
+    {
+        isCoroutineStarted = true;
+        yield return new WaitForSeconds(damageInterval);
+        isReadyToReceiveDamage = true;
+        isCoroutineStarted = false;
+        Debug.Log("Was Called");
+        yield return null;
+    }
+}
