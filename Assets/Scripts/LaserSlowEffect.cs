0b7cb5ec383065fdc1b89b4c98e5d84d00c99b57
Pofay <gianCarlo.gilos@gmail.com>
2021-09-18 16:47:34 +0800

    Created duplicate SlowEffect specific for the LaserTurret

---
+++ Assets/Scripts/LaserSlowEffect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LaserSlowEffect : IStatusEffect
+{
+    public string Id => "LASER_SLOW_EFFECT";
+    public float RunningDuration { get; private set; }
+    public bool IsActive { get; private set; }
+
+    private float slowPercentage;
+    private float duration;
+
+    public LaserSlowEffect(float slowPercentage, float duration)
+    {
+        this.slowPercentage = slowPercentage;
+        this.duration = duration;
+        this.RunningDuration = duration;
+        this.IsActive = false;
+    }
+
+    public IEnumerator ApplyEffect(GameObject target)
+    {
+        RefreshDuration();
+        var targetMovement = target.GetComponent<EnemyMovement>();
+
+        IsActive = true;
+        targetMovement.AddModifier(Id, -(targetMovement.BaseSpeed * (slowPercentage)));
+
+        while (RunningDuration > 0)
+        {
+            RunningDuration -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        RemoveAppliedEffect(targetMovement);
+        yield return null;
+    }
+
+    private void RemoveAppliedEffect(EnemyMovement targetMovement)
+    {
+        targetMovement.RemoveModifier(Id);
+        IsActive = false;
+    }
+
+    public void RefreshDuration()
+    {
+        RunningDuration = duration;
+    }
+}
