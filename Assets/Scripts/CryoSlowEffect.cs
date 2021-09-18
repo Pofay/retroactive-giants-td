5a83fbb78a49bbcc6714641ae75b1b97f2f12161
Pofay <gianCarlo.gilos@gmail.com>
2021-09-18 16:04:36 +0800

    Updated CryoSlowEffect to implement RefreshDuration to prevent resetting of running coroutine

--- Assets/Scripts/CryoSlowEffect.cs
+++ Assets/Scripts/CryoSlowEffect.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class CryoSlowEffect : IStatusEffect
 {
     public string Id => "CRYO_SLOW_EFFECT";
+    public bool IsActive { get; private set; }
 
     private float slowPercentage;
     private float duration;
@@ -10,28 +12,40 @@ public class CryoSlowEffect : IStatusEffect
 
     public float RunningDuration { get; private set; }
 
-    
-    public CryoSlowEffect(float slowPercentage, float duration)
+    public CryoSlowEffect(GameObject target, float slowPercentage, float duration)
     {
+        this.targetMovement = target.GetComponent<EnemyMovement>();
         this.slowPercentage = slowPercentage;
         this.duration = duration;
         this.RunningDuration = duration;
+        this.IsActive = false;
     }
 
-    public void Tick(GameObject target, float time)
+    public IEnumerator ApplyEffect(GameObject target)
     {
-        targetMovement = target.GetComponent<EnemyMovement>();
-        RunningDuration -= time;
-        targetMovement.CurrentSpeed = targetMovement.BaseSpeed * (1f - slowPercentage);
+        RefreshDuration();
+
+        IsActive = true;
+        targetMovement.AddModifier(Id, -(targetMovement.BaseSpeed * (slowPercentage)));
+
+        while (RunningDuration > 0)
+        {
+            RunningDuration -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        RemoveAppliedEffect(targetMovement.gameObject);
+        yield return null;
+    }
+
+    public void RemoveAppliedEffect(GameObject target)
+    {
+        targetMovement.RemoveModifier(Id);
+        IsActive = false;
     }
 
     public void RefreshDuration()
     {
         RunningDuration = duration;
-    }
-
-    public void ResetStatus()
-    {
-        targetMovement.ResetSpeed();
     }
 }
