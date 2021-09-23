6f54deb8a52528f228d7bf353993e9da5357ddb0
Pofay <gianCarlo.gilos@gmail.com>
2021-09-23 22:05:26 +0800

    Created Base StatusEffectFactory as baseline for all scriptableobject status effects

---
+++ Assets/Scripts/StatusEffectFactory.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public abstract class StatusEffectFactory : ScriptableObject
+{
+    public virtual string EffectId => "BASE_EFFECT";
+
+
+    public abstract IStatusEffect CreateEffect();
+}
