696fdc736eeb41d14f7a97d691c8b3b327e55d48
Pofay <gianCarlo.gilos@gmail.com>
2021-09-23 22:05:54 +0800

    Added Implementations for various StatusEffects for specific turrets

---
+++ Assets/Scripts/BurnEffectFactory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Status Effects", menuName = "Status Effects/Flamethrower Burn")]
+public class BurnEffectFactory : StatusEffectFactory
+{
+    public override string EffectId => "FLAMETHROWER_EFFECT";
+
+    [Header("Flamethrower Burn Effect Values")]
+    [Range(3f, 9f)]
+    public float damage;
+    [Range(0f, 3f)]
+    public float tickInterval;
+    [Range(1f, 6f)]
+    public float duration;
+
+    public override IStatusEffect CreateEffect()
+    {
+        return new BurnEffect(damage, tickInterval, duration);
+    }
+}
