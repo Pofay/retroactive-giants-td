34dad162a5b6712763ecb97affee608176a53472
Pofay <gianCarlo.gilos@gmail.com>
2021-09-24 20:59:48 +0800

    Removed attributes since StatusEffectFactory contains data regarding slow status effect

--- Assets/Scripts/SlowingImpactEffect.cs
+++ Assets/Scripts/SlowingImpactEffect.cs
@@ -2,12 +2,6 @@
 
 public class SlowingImpactEffect : MonoBehaviour, IImpactEffect
 {
-    [Header("Slowing Effect settings")]
-    public float slowDuration = 3f;
-
-    [Range(0f, 1f)]
-    public float slowPercentage = 0.3f;
-
     public StatusEffectFactory statusEffectFactory;
 
     public void ApplyEffect(Transform target)
