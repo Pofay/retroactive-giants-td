2e7e3956239c7889f1efce31ba39bc429982af61
Pofay <gianCarlo.gilos@gmail.com>
2021-10-06 21:06:38 +0800

    Added EnemyDeathHandler to perform cleanup when health reaches 0

---
+++ Assets/Scripts/EnemyDeathHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDeathHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    private bool hasPerformedCleanup;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        var health = GetComponent<EnemyHealth>();
+        health.OnDeath += PerformCleanup;
+        hasPerformedCleanup = false;
+    }
+
+    private void PerformCleanup()
+    {
+        if (!hasPerformedCleanup)
+        {
+            StartCoroutine(PerformDeathSequence());
+        }
+    }
+
+    private IEnumerator PerformDeathSequence()
+    {
+        hasPerformedCleanup = true;
+        animator.SetTrigger("Death");
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<EnemyHealth>().enabled = false;
+        GetComponentInChildren<EnemyHealthUI>().enabled = false;
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+        yield return new WaitForEndOfFrame();
+    }
+}
