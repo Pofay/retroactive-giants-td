139163c69707064ac87afaa6908dc9c1d4cb949a
Pofay <gianCarlo.gilos@gmail.com>
2021-10-13 19:49:00 +0800

    Refactored and added currency increase step to Death sequence

--- Assets/Scripts/EnemyDeathHandler.cs
+++ Assets/Scripts/EnemyDeathHandler.cs
@@ -5,12 +5,14 @@ using UnityEngine.AI;
 public class EnemyDeathHandler : MonoBehaviour
 {
     private Animator animator;
+    private EnemyWorth enemyWorth;
 
     private bool hasStartedDeathSequence;
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        enemyWorth = GetComponent<EnemyWorth>();
         var health = GetComponent<EnemyHealth>();
         health.OnDeath += StartDeathSequence;
         hasStartedDeathSequence = false;
@@ -27,12 +29,28 @@ public class EnemyDeathHandler : MonoBehaviour
     private IEnumerator PerformDeathSequence()
     {
         hasStartedDeathSequence = true;
-        animator.SetTrigger("Death");
-        GetComponent<SphereCollider>().enabled = false;
-        GetComponent<NavMeshAgent>().enabled = false;
-        GetComponent<EnemyHealth>().enabled = false;
+        PerformDeathAnimation();
+        DisableCollisionAndNavigation();
+        HideHealthbar();
+        enemyWorth.AddCurrencyToPlayer();
         yield return new WaitForSeconds(2f);
         gameObject.SetActive(false);
         yield return new WaitForEndOfFrame();
+    }
+
+    private void HideHealthbar()
+    {
+        GetComponent<EnemyHealth>().enabled = false;
+    }
+
+    private void DisableCollisionAndNavigation()
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<NavMeshAgent>().enabled = false;
+    }
+
+    private void PerformDeathAnimation()
+    {
+        animator.SetTrigger("Death");
     }
 }
