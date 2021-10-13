aa8ea1ea17b113784a088b05e9743fb4b3e39493
Pofay <gianCarlo.gilos@gmail.com>
2021-10-13 20:19:14 +0800

    Play Audio when enemy reaches the end

--- Assets/Scripts/LivesRemover.cs
+++ Assets/Scripts/LivesRemover.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class LivesRemover : MonoBehaviour
 {
     private PlayerStats playerStats;
+    private AudioSource audioSource;
 
     void Awake()
     {
         playerStats = FindObjectOfType<PlayerStats>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         RemoveLives(1);
+        audioSource.Play();
         other.gameObject.SetActive(false);
     }
 
