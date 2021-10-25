244f7b2fc0693b40636ca3e68d190923841838ba
Pofay <gianCarlo.gilos@gmail.com>
2021-10-25 20:44:48 +0800

    Created PlayerStatisticsData to hold starting values for lives and currency. Also moved each Level setting to dedicated folder

---
+++ Assets/Scripts/PlayerStatisticsData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level Starting Stats", menuName = "Level Settings/Starting Stats")]
+public class PlayerStatisticsData : ScriptableObject
+{
+    public int startingCurrency;
+    public int startingLives;
+}
