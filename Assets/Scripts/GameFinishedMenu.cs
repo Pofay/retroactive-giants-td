8eb559426f11783faae845f030af57ae0bedd233
Pofay <gianCarlo.gilos@gmail.com>
2021-11-10 22:57:54 +0800

    Added script to control GameFinishedScreen

---
+++ Assets/Scripts/GameFinishedMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameFinishedMenu : MonoBehaviour
+{
+    public GameObject gameFinishedScreen;
+    public Button mainMenuButton;
+    public Button quitButton;
+
+    private SceneFader sceneFader;
+
+    void Awake()
+    {
+        sceneFader = FindObjectOfType<SceneFader>();
+        gameFinishedScreen.SetActive(false);
+        var unlocker = FindObjectOfType<LevelUnlocker>();
+        unlocker.OnLastLevelCompleted += ShowGameFinishedScreen;
+        mainMenuButton.onClick.AddListener(() => ToMainMenu());
+        quitButton.onClick.AddListener(() => Quit());
+    }
+
+    private void ShowGameFinishedScreen()
+    {
+        gameFinishedScreen.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1f;
+        gameFinishedScreen.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void ToMainMenu()
+    {
+        Unpause();
+        sceneFader.FadeTo("MainMenu");
+    }
+}
