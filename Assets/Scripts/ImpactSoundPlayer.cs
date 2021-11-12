21e82fb5e1fd9593399a4afa0edd231d34b7a1fd
Pofay <gianCarlo.gilos@gmail.com>
2021-11-12 23:08:58 +0800

    Introduced sound player object to prevent bullets from having NPEs when target gets disabled

---
+++ Assets/Scripts/ImpactSoundPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class ImpactSoundPlayer : MonoBehaviour
+{
+    public ImpactSound[] impactSFX;
+
+    public static ImpactSoundPlayer instance;
+
+    void Awake()
+    {
+        instance = this;
+        foreach (var s in impactSFX)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        var s = Array.Find(impactSFX, sound => sound.name.Equals(name));
+        s.source.Play();
+    }
+}
