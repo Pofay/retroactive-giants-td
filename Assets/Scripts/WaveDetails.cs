0b2ad92b3a6299c78a627d655d12ef7d754fe8f9
Pofay <gianCarlo.gilos@gmail.com>
2022-04-03 22:17:35 +0800

    Updated to use AssetReference instead of GameObject to use Addressable system for instantiation

--- Assets/Scripts/WaveDetails.cs
+++ Assets/Scripts/WaveDetails.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 [CreateAssetMenu(menuName = "Enemies/Wave Details", order = 1)]
 public class WaveDetails : ScriptableObject
 {
-    public GameObject enemyPrefab;
+    public AssetReference enemyAssetReference;
     public int enemiesPerRound = 10;
     public int numberOfRounds = 0;
     public float timeBetweenSpawns = 5;
